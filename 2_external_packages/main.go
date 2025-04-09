f61eebd1d935350dafcddb74467acfddf5a1d9e0
Emanuel Vogt <contatoemanuel3@gmail.com>
2025-04-09 15:27:57 -0300

    using external package for validating email

--- 2_external_packages/main.go
+++ 2_external_packages/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing from main file")
 	auxiliar.Write()
+
+	error := checkmail.ValidateFormat("examplemailcom")
+	fmt.Println(error)
+
 }
